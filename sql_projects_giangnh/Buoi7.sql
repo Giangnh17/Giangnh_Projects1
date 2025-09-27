bd6c9b765146262c4ad00d81ba4ce86f4dd4d0df
Giangnh17 <gianghuong2829@gmail.com>
2025-09-27 22:36:17 +0700

    Update Buoi7.sql

--- sql_projects_giangnh/Buoi7.sql
+++ sql_projects_giangnh/Buoi7.sql
@@ -12,20 +12,29 @@ VALUES	(113, 'Ipad', '500', 'Stationery');
 -- 3. Thêm đơn hàng ID = 1021 do khách hàng có ID = 1 đặt ngày 2025-07-28​
 SELECT * FROM Orders;
 INSERT INTO Orders(OrderID, CustomerID, OrderDate)
-VALUES (1021, 1, '2025-07-28​');
+VALUES (1021, 1, '2025-07-28'​);
 
 -- 4. Thêm chi tiết đơn hàng có ID = 1021 gồm 2 sản phẩm có ID = 1​
+-- Trong DB không có sản phẩm có ID = 1 nên thêm product ID = 1
 SELECT * FROM OrderDetail;
+INSERT INTO Products(ProductID, ProductName, Price, Category)
+VALUES	(1, 'Eke', '5', 'Stationery');
 INSERT INTO OrderDetail(OrderDetailID, OrderID, ProductID, Quantity)
-VALUES (41, 1021, 1, 2);
+VALUES (1021, 1021, 1, 2);
 
 -- 5. Cập nhật giá của sản phẩm "Laptop" thành 1500​
 UPDATE Products
 SET Price = 1500 WHERE ProductName = 'Laptop';
 
 -- 6. Xóa đơn hàng và chi tiết đơn hàng của người dùng có ID = 1​
-DELETE FROM Orders WHERE CustomerID = 1;
-DELETE FROM OrderDetail WHERE OrderID = (SELECT OrderID FROM Orders WHERE CustomerID = 1);
+ALTER TABLE Orders
+ADD isDeleted BIT NOT NULL DEFAULT 0;
+
+ALTER TABLE OrderDetail
+ADD isDeleted BIT NOT NULL DEFAULT 0;
+
+UPDATE Orders SET isDeleted = 1 WHERE CustomerID = 1;
+UPDATE OrderDetail SET isDeleted = 1 WHERE OrderID IN (SELECT OrderID FROM Orders WHERE isDeleted = 1) ;
 
 -- 7. Hiển thị danh sách đơn hàng, bao gồm:​ Mã đơn hàng​, Ngày đặt hàng​, Tên khách hàng
 SELECT OrderID, OrderDate, Name
