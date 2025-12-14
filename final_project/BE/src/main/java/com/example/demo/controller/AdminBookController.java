a20583bd383be1d15d9d75b561f0c8285ae5494b
Giangnh17 <gianghuong2829@gmail.com>
2025-12-14 09:27:19 +0700

    update end_project

--- final_project/BE/src/main/java/com/example/demo/controller/AdminBookController.java
+++ final_project/BE/src/main/java/com/example/demo/controller/AdminBookController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin/product")
+@RequestMapping("/admin/books")
 public class AdminBookController {
 
     @Autowired
